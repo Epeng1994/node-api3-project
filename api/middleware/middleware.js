59619434bcbc129e553258e4b7da65b28c058bbd
unknown <epeng1994@gmail.com>
2022-06-08 18:27:45 -0400

    MVP

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,56 @@
+const postsModel = require('../posts/posts-model')
+const usersModel = require('../users/users-model')
+
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  let time =  new Date()
+  console.log(req.method, req.url, time)
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  usersModel.getById(req.params.id)
+  .then(result=>{
+    if(result){
+      req.user = result
+      next()
+    }else{
+      res.status(404).json({ message: "user not found" })
+    }
+  })
+  .catch(error=>{
+    res.status(500).json('Bad request')
+    return
+  })
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const {name} = req.body
+  if(name == null || name.trim() == ''){
+    res.status(400).json({ message: "missing required name field" })
+    return
+  }else{
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  if(req.body.text == null || req.body.text.trim() === ''){
+    res.status(400).json({ message: "missing required text field" })
+    console.log('passed')
+    return
+  }else{
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const postsModel = require('../posts/posts-model')
+const usersModel = require('./users-model')
+const {validateUserId,validateUser,validatePost} = require('../middleware/middleware')
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
@@ -7,38 +10,82 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  usersModel.get()
+    .then(result=>{
+      res.json(result)
+    })
+    .catch(error=>{
+      res.status(500).json('Bad request')
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.json(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  usersModel.insert(req.body)
+    .then(result=>{
+      res.json(result)
+    })
+    .catch(error=>{
+      res.status(500).json('Bad request')
+    })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  usersModel.update(req.user.id, req.body)
+    .then(result=>{
+      res.json(result)
+    })
+    .catch(error=>{
+      res.status(500).json('Bad request') 
+    })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  usersModel.remove(req.user.id)
+    .then(result=>{
+      res.json(req.user)
+    })
+    .catch(error=>{
+      res.status(500).json('Bad request') 
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  usersModel.getUserPosts(req.user.id)
+    .then(result=>{
+      res.json(result)
+    })
+    .catch(error=>{
+      res.status(500).json('Bad request') 
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  postsModel.insert({text:req.body.text, user_id:req.user.id})
+    .then(result=>{
+      res.json(result)
+    })
+    .catch(error=>{
+      res.status(500).json('Bad request') 
+    })
 });
 
 // do not forget to export the router
+module.exports = router
